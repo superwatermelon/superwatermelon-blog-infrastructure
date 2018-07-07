89361a877b2a87dccb958c07d41c753056b177fb
Stuart Wakefield <me@stuartwakefield.co.uk>
2018-07-07 08:49:46 +0100

    Add ECR for app and nginx docker images

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,7 @@
-resource "aws_ecr_repository" "blog_procedural_terrain_generation" {
-  name = "superwatermelon-blog-procedural-terrain-generation"
+resource "aws_ecr_repository" "blog_procedural_terrain_generation_app" {
+  name = "superwatermelon-blog-procedural-terrain-generation-app"
+}
+
+resource "aws_ecr_repository" "blog_procedural_terrain_generation_nginx" {
+  name = "superwatermelon-blog-procedural-terrain-generation-nginx"
 }
